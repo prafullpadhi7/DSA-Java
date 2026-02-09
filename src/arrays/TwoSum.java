b350f1fdcfc74f83c801f9ec0d8967fed2f654f0
prafullpadhi7 <prafullpadhi7@gmail.com>
2026-02-10 02:45:38 +0530

    Day 2: Two Sum

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,22 @@
+package arrays;
+/*
+LeetCode: 1. Two Sum
+Difficulty: Easy
+Topic: Arrays
+Time: O(n^2)
+Space: O(1)
+*/
+public class TwoSum {
+	 public int[] twoSum(int[] nums, int target) {
+	     
+	        for(int i = 0 ; i<nums.length ; i++){
+	            for(int j=i+1 ; j<nums.length; j++){
+	                 if(nums[i]+nums[j]==target){
+	               return new int[]{i,j};
+	            }
+	            }
+	           
+	        }
+	        return new int[]{};
+	    }
+}
