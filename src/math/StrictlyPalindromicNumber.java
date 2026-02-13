a59b19118bab4c46d7099dbb2ae5b9646f46355f
prafullpadhi7 <prafullpadhi7@gmail.com>
2026-02-13 18:50:40 +0530

    Day 4 : Strictly Palindromic Number

---
+++ src/math/StrictlyPalindromicNumber.java
@@ -0,0 +1,15 @@
+package math;
+
+/*
+LeetCode: 2396. Strictly Palindromic Number
+Difficulty: Medium
+Topic: math
+Time: O(1)
+*/
+
+public class StrictlyPalindromicNumber {
+	
+	 public boolean isStrictlyPalindromic(int n) {
+	        return false;
+	    }
+}
